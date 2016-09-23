786e2b6746fe1839c8e853c470126a073bfd58bb
Daniel Schaaff <dschaaff@knuedge.com>
2016-09-23 10:09:54 -0700

    fix puppet agent package name for macos

--- manifests/params.pp
+++ manifests/params.pp
@@ -92,8 +92,7 @@ class puppet::params {
     'Darwin': {
       $puppet_agent_service         = 'com.puppetlabs.puppet'
       $puppet_agent_version         = '3.8.7'
-      $puppet_agent_package         = 'puppet-3.8.6.dmg'
-      $puppet_facter_package        = 'facter-2.4.6.dmg'
+      $puppet_agent_package         = 'puppet'
       $package_provider             = 'gem'
       $puppet_conf                  = '/etc/puppet/puppet.conf'
       $puppet_vardir                = '/var/lib/puppet'
