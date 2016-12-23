d1671c31a4e6deb3ab9cf2dae96057433f2a5a4c
Daniel Schaaff <dschaaff@knuedge.com>
2016-12-23 13:59:03 -0800

    update repo for puppet 4

--- manifests/repo/puppetlabs.pp
+++ manifests/repo/puppetlabs.pp
@@ -5,37 +5,28 @@
 class puppet::repo::puppetlabs() {
 
   if($::osfamily == 'Debian') {
-    Apt::Source {
+    Apt::Source { 'puppetlabs-pc1'
       location    => 'http://apt.puppetlabs.com',
-      key         => {
+      repos    => 'PC1',
+      key      => {
         'id'     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
         'server' => 'pgp.mit.edu',
       },
     }
-    apt::source { 'puppetlabs':      repos => 'main' }
-    apt::source { 'puppetlabs-deps': repos => 'dependencies' }
   } elsif $::osfamily == 'Redhat' {
     if $::operatingsystem == 'Fedora' {
-      $ostype='fedora'
-      $prefix='f'
+      $ostype = 'fedora'
+      $prefix = 'f'
     } else {
-        $ostype='el'
-        $prefix=''
+        $ostype = 'el'
+        $prefix = $::operatingsystemmajrelease
     }
-    yumrepo { 'puppetlabs-deps':
-      baseurl  => "http://yum.puppetlabs.com/${ostype}/${prefix}\$releasever/dependencies/\$basearch",
-      descr    => 'Puppet Labs Dependencies $releasever - $basearch ',
-      enabled  => '1',
+    yumrepo {'puppetlabs-pc1':
+      baseurl  => "https://yum.puppetlabs.com/${ostype}/${version}/PC1/\$basearch",
+      descr    => 'Puppetlabs PC1 Repository',
+      enabled  => true,
       gpgcheck => '1',
-      gpgkey   => 'https://yum.puppetlabs.com/RPM-GPG-KEY-puppet',
-    }
-
-    yumrepo { 'puppetlabs':
-      baseurl  => "http://yum.puppetlabs.com/${ostype}/${prefix}\$releasever/products/\$basearch",
-      descr    => 'Puppet Labs Products $releasever - $basearch',
-      enabled  => '1',
-      gpgcheck => '1',
-      gpgkey   => 'https://yum.puppetlabs.com/RPM-GPG-KEY-puppet',
+      gpgkey   => 'https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs'
     }
   } else {
     fail("Unsupported osfamily ${::osfamily}")
